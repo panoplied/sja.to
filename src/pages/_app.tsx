795e75835cc46190e2862c9dfb0ec2b486698f0b
panoplied <panoplied@protonmail.com>
2022-08-23 02:07:55 +0300

    Add tRPC with defaults

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next';
+import type { AppProps } from 'next/app';
+import { AppRouter } from './api/trpc/[trpc]';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+
+  // reference for vercel.com
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // assume localhost
+  return `https://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export default withTRPC<AppRouter>({
+  config() {
+    return {
+      url: `${getBaseUrl()}/api/trpc`,
+    };
+  },
+  ssr: false,
+})(MyApp);

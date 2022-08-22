1d415b0bb9c78bcfeb5828a1d5b0094b1a910187
panoplied <panoplied@protonmail.com>
2022-08-22 23:17:04 +0300

    Add boilerplate markup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,15 @@ const Home: NextPage = () => {
       <Head>
         <title>СЖАТО</title>
         <meta name="description" content="URL shortener" />
+
+        <link rel="preload" href="/fonts/Glass_TTY_VT220.ttf" as="font" crossOrigin="anonymous" type="font/ttf" />
+
+        {/* TODO: make proper favicons for everything */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>СЖАТО</h1>
+      <main className="bg-black min-h-screen grid place-items-center">
+        <h1 className="font-glassTTYVT220 text-white text-[80px]">СЖАТО</h1>
       </main>
     </div>
   );

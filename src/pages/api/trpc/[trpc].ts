84436cd22927b231548474c2d80de8864c6a70ed
panoplied <panoplied@protonmail.com>
2022-08-23 15:59:51 +0300

    Add query and mutation for tRPC

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,18 +2,37 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 
+import { prisma } from '../../../db/prisma-client';
+
 export const appRouter = trpc
   .router()
-  .query('hello', {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `hello ${input?.text ?? 'world'}`,
-      };
+  .query("fetchUrl", {
+    input: z.object({ slug: z.string() }),
+    async resolve({ input }) {
+      const urlCount = await prisma.Url.count({
+        where: {
+          slug: {
+            equals: input.slug,
+          },
+        },
+      });
+
+      return { used: urlCount > 0 };
+    },
+  })
+  .mutation("addUrl", {
+    input: z.object({ slug: z.string(), url: z.string() }),
+    async resolve({ input }) {
+      try {
+        await prisma.Url.create({
+          data: {
+            slug: input.slug,
+            url: input.url,
+          },
+        });
+      } catch (err) {
+        console.error(err);
+      }
     },
   });
 

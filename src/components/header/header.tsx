682042ee08afc63be609ea25d91bf89d5ad6fda8
panoplied <panoplied@protonmail.com>
2022-08-24 18:29:23 +0300

    Add Header component and animate it; Refactoring

---
+++ src/components/header/header.tsx
@@ -0,0 +1,31 @@
+import ASCIIAnimation from "../ascii-animation";
+
+const animLeft = [
+  <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
+  <><span className="amber">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
+  <><span className="text-stone-900">{'>'}</span><span className="amber">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
+  <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="amber">{'>'}</span></>,
+  <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
+];
+
+const animRight = [
+  <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
+  <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="amber">{'<'}</span></>,
+  <><span className="text-stone-900">{'<'}</span><span className="amber">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
+  <><span className="amber">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
+  <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
+];
+
+function Header() {
+  return(
+    <>
+        <h1 className="font-glassTTYVT220 text-[40px]">
+          <ASCIIAnimation frames={animLeft} rate={300} />
+          {' '}<span className="amber">TESTING</span> {' '}
+          <ASCIIAnimation frames={animRight} rate={300} />
+        </h1>
+    </>
+  );
+}
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,30 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import UrlForm from '../components/url-form';
+import Scanlines from '../components/scanlines';
+import Header from '../components/header';
 
 const Home: NextPage = () => {
   return (
     <div>
+
       <Head>
-        <title>СЖАТО</title>
+        <title>СЖАТО - URL shortener</title>
         <meta name="description" content="URL shortener" />
 
         <link rel="preload" href="/fonts/Glass_TTY_VT220.ttf" as="font" crossOrigin="anonymous" type="font/ttf" />
+        <link rel="preload" href="/fonts/Orden_Regular.woff2" as="font" crossOrigin="anonymous" type="font/woff" />
 
         {/* TODO: make proper favicons for everything */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="bg-black min-h-screen grid place-items-center">
-        <h1 className="font-glassTTYVT220 text-white text-[80px]">СЖАТО</h1>
+        <Scanlines />
+        <Header />
         <UrlForm />
       </main>
+
     </div>
   );
 }

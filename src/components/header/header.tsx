12fb48f87a3d91ac578c19bfc74e2a096d9e80be
panoplied <panoplied@protonmail.com>
2022-08-27 04:49:45 +0300

    Update colors in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,17 +2,17 @@ import ASCIIAnimation from "../ascii-animation";
 
 const animLeft = [
   <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
-  <><span className="amber">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
-  <><span className="text-stone-900">{'>'}</span><span className="amber">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
-  <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="amber">{'>'}</span></>,
+  <><span className="emerald">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
+  <><span className="text-stone-900">{'>'}</span><span className="emerald">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
+  <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="emerald">{'>'}</span></>,
   <><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span><span className="text-stone-900">{'>'}</span></>,
 ];
 
 const animRight = [
   <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
-  <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="amber">{'<'}</span></>,
-  <><span className="text-stone-900">{'<'}</span><span className="amber">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
-  <><span className="amber">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
+  <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="emerald">{'<'}</span></>,
+  <><span className="text-stone-900">{'<'}</span><span className="emerald">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
+  <><span className="emerald">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
   <><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span><span className="text-stone-900">{'<'}</span></>,
 ];
 
@@ -22,11 +22,11 @@ function Header() {
         <h1 className="mt-4 font-glassTTYVT220 text-[40px]">
           <ASCIIAnimation frames={animLeft} rate={300} />
           <span className="font-sans">&nbsp;&nbsp;</span>
-          <span className="amber">СЖАТО</span>
+          <span className="emerald">СЖАТО</span>
           <span className="font-sans">&nbsp;&nbsp;</span>
           <ASCIIAnimation frames={animRight} rate={300} />
         </h1>
-        <h2 className="mt-4 mb-12 crtFont slate">
+        <h2 className="mt-[10px] mb-[80px] crtFont slate">
           URL shortener
         </h2>
     </>
